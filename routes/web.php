656aee1897649fb71e2927227b2f09e8b2c337ad
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-05-25 20:11:13 +0200

    inizio

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/libri', [BookController::class, 'index'])->name('books.index');
+Route::get('/libri/crea', [BookController::class, 'create'])->name('books.create');
+Route::post('/libri/salva', [BookController::class, 'store'])->name('books.store');
+Route::get('/libri/{book}/dettagli', [BookController::class, 'show'])->name('books.show');
