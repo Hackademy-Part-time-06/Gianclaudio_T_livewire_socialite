c534c2a73ed57266bce6407b0bc0f1de1a363805
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-05-26 18:36:25 +0200

    fine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,21 @@
 
         <div class="container">
             <div class="row ">
-                <div class="col-mb-6 d-flex justify-content-center">
+                <div class="col-mb-6 d-flex justify-content-center mt-5">
                     <button class="">
                     <a href="{{route('books.index')}}">Lezione</a>
+                     </button>   
+                      
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row ">
+                <div class="col-mb-6 d-flex justify-content-center mt-5">
+                    <button class="">
+                    <a href="{{route('books.create')}}">Crea</a>
                      </button>   
                       
                 </div>
