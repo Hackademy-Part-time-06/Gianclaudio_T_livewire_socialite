8a4a3e28d53de4013466ac99e7336851d865a544
Gianclaudio <gianclaudiotarantini@gmail.com>
2023-05-26 18:30:06 +0200

    fine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <div class="container">
+            <div class="row ">
+                <div class="col-mb-6 d-flex justify-content-center">
+                    <button class="">
+                    <a href="{{route('books.index')}}">Lezione</a>
+                     </button>   
+                      
+                </div>
+                
+            </div>
+        </div>
         
      
     <body class="antialiased">
